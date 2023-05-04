5a52a428138eda89f6b28bc6d94f64ec01be7abe
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-04 15:36:33 +0900

    [✨기능] StoreItem 리스트 렌더링
    - items.json에서 데이터를 뽑아와 Array.map()으로 리스트 렌더링을 해줌

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+// 더미 데이터 불러오기
+import storeItems from "../data/items.json";
+
 export function Store() {
   return(
-    <h1>Store</h1>
+    <>
+      <h1>Store</h1>
+      <Row>
+        {
+          //리스트렌더링
+          storeItems.map(item => (
+            // 객체에서 값을 불러오기 때문에 JSON.stringify()로 문자화
+            <Col>{JSON.stringify(item)}</Col>
+          ))
+        }
+      </Row>
+    </>
   )
 }
