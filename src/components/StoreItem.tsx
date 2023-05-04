a58a0cee3db8c3b74898156c9c804446e4dd6d58
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-04 17:41:23 +0900

    [🎨스타일] StoreItem 카드 컴포넌트 버튼 UI 완성
    - 장바구니 담기 버튼
    - +, - 버튼

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from '../utils/_index';
 
 //prop의 타입 정하기
@@ -10,8 +10,11 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
+
+  const quantity = 0;
+
   return(
-  <Card>
+  <Card className="h-100">
     <Card.Img 
       variant='top' src={imgUrl} 
       height="200px"
@@ -22,6 +25,30 @@ export function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
         <span className="fs-2">{name}</span>
         <span className="ms-2 text-muted">{formatCurrency(price)}</span>
       </Card.Title>
+      <div className="mt-auto">
+        {/* 조건부 렌더링 */}
+        {quantity === 0 ? (
+          <Button className="w-100">+ 장바구니 담기</Button>
+        ) : 
+        <div 
+          className="d-flex align-items-center flex-column"
+          style={{gap: "0.5rem"}}  
+        >
+          <div 
+            className="d-flex align-items-center justify-content-center"
+            style={{gap: "0.5rem"}}  
+          >
+            <Button>-</Button>
+            <div>
+              <span className="fs-3">{quantity}</span> in cart
+            </div>
+            <Button>+</Button>
+          </div>
+          <Button  variant="danger" size="sm">
+            Remove
+          </Button>
+        </div>}
+      </div>
     </Card.Body>
   </Card>
   )
