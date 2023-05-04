686e493a81bad91435ce85e156d393e24f2f1fe1
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-04 16:20:17 +0900

    [🎨스타일] StoreItem 카드 컴포넌트 UI 완성
    - UI 완성
    - props 데이터 바인딩

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,5 @@
+import { Card } from "react-bootstrap";
+
 //prop의 타입 정하기
 type StoreItemProps = {
   id: number,
@@ -8,7 +10,18 @@ type StoreItemProps = {
 
 export function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
   return(
-  <>
-  </>
+  <Card>
+    <Card.Img 
+      variant='top' src={imgUrl} 
+      height="200px"
+      style={{ objectFit: "cover" }}
+    />
+    <Card.Body className="d-flex flex-column">
+      <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+        <span className="fs-2">{name}</span>
+        <span className="ms-2 text-muted">{price}</span>
+      </Card.Title>
+    </Card.Body>
+  </Card>
   )
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,6 @@
 import { Col, Row } from "react-bootstrap";
-// 더미 데이터 불러오기
 import storeItems from "../data/items.json";
+import { StoreItem } from '../components/_index';
 
 export function Store() {
   return(
@@ -8,10 +8,11 @@ export function Store() {
       <h1>Store</h1>
       <Row xs={1} md={2} lg={3} className="g-3">
         {
-          //리스트렌더링
           storeItems.map(item => (
-            // 객체에서 값을 불러오기 때문에 JSON.stringify()로 문자화
-            <Col>{JSON.stringify(item)}</Col>
+            // 객체를 StoreItem의 props로 전달
+            <Col key={item.id}>
+              <StoreItem {...item}/>
+            </Col>
           ))
         }
       </Row>
